06e0072958deffadf54bfedaaa8903ee6dc026eb
Blake Sheehan <blaketsheehan@gmail.com>
2024-12-07 12:34:42 -0700

    Added pages from sonias repository that adds a delete functionality to the enrolled students table.

--- university-management-frontend/src/App.js
+++ university-management-frontend/src/App.js
@@ -1,16 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import StudentEnrollment from './pages/StudentEnrollment';
+import StudentList from './pages/StudentList';
 
 function App() {
     return (
         <Router>
             <Routes>
-                {/* Use `element` to render the component */}
+                {/* Route for enrolling a student */}
                 <Route path="/enroll" element={<StudentEnrollment />} />
+                
+                {/* Route for displaying the student list */}
+                <Route path="/students" element={<StudentList />} />
             </Routes>
         </Router>
     );
 }
+
 
 export default App;
 
